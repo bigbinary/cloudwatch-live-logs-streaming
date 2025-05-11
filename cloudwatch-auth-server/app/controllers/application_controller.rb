0d262046434795199cbb14ebf217cfe61e4c0335
Unnikrishnan KP <unnikrishnan.kp@bigbinary.com>
2025-05-11 22:33:56 +0530

    Auth server removed

--- cloudwatch-auth-server/app/controllers/application_controller.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationController < ActionController::API
-  include Authenticatable
-  before_action :authenticate_request
-end
--- cloudwatch-auth-server/app/controllers/health_controller.rb
+++
@@ -1,8 +0,0 @@
-class HealthController < ApplicationController
-  skip_before_action :authenticate_request, only: [:check]
-
-  def check
-    render json: { status: 'ok' }, status: :ok
-  end
-end
-
